a6110c38afba1836b56f7d116f884e12dbf688af
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2025-10-23 12:22:55 +0100

    :)

--- frontend/public/godot/index.service.worker.js
+++ frontend/public/godot/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761209949|842817984';
+const CACHE_VERSION = '1761218533|90234443';
 /** @type {string} */
 const CACHE_PREFIX = 'World of Nads-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
