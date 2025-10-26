a87f5d7543887294cf5540dadb60dff2de102bde
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2025-10-26 10:10:06 +0100

    :)

--- frontend/src/components/ThreeScene.tsx
+++ frontend/src/components/ThreeScene.tsx
@@ -52,8 +52,8 @@ export const ThreeScene: React.FC<Props> = ({ twitter, wallets, earned, onLogout
 
       <OrbitControls
         enableZoom={false}
-        minAzimuthAngle={-Math.PI / 2}
-        maxAzimuthAngle={Math.PI / 2}
+        minAzimuthAngle={-Math.PI / 18}
+        maxAzimuthAngle={Math.PI / 18}
         minPolarAngle={Math.PI / 4}
         maxPolarAngle={(3 * Math.PI) / 4}
       />
