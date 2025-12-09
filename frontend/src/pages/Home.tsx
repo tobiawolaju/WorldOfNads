cd7ec7488d39f3cb0195e4a844ea5788e37b2089
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2025-12-09 01:12:04 +0100

    :0

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -197,7 +197,7 @@ const Home: React.FC = () => {
           className="absolute bottom-0 w-[80vw] h-[80vw] md:w-[80vh] md:h-[80vh]"
         >
           <Rive
-            src="https://cdn.rive.app/animations/vehicles.riv"
+            src="nad.riv"
             stateMachines="bumpy"
           />
         </div>
