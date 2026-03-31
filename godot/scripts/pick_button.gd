9913801b536269a5fbf14b9188109986585aef18
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-03-31 13:11:58 +0100

    :P

--- godot/scripts/pick_button.gd
+++ godot/scripts/pick_button.gd
@@ -1,15 +1,8 @@
-extends TextureButton
+extends Button
 
 func _ready():
 	# Connect the pressed signal
 	pressed.connect(_on_pressed)
-
-	# Position button 100px above bottom and 100px left from right edge
-	var viewport_size = get_viewport().get_visible_rect().size
-	global_position = Vector2(
-		viewport_size.x - 150,   # 100px left from right edge
-		viewport_size.y - 150    # 100px above bottom edge
-	)
 
 func _on_pressed():
 	var ev := InputEventAction.new()
