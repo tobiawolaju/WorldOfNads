364994e245c12b33949db863aebc630627bb6ac5
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-04-04 13:17:33 +0100

    :p0

--- godot/scripts/Main.gd
+++ godot/scripts/Main.gd
@@ -47,7 +47,7 @@ var _world_env_duplicated := false
 var match_duration_seconds := 180.0
 var match_time_left := 180.0
 var match_is_running := false
-var fog_start_color: Color =  Color8(215, 100, 255)
+var fog_start_color: Color =  Color8(85, 205, 255)
 var fog_end_color: Color = Color8(255, 100, 215)
 
 # --- FALLBACK LOGIC ---
