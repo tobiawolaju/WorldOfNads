5bcbd486017bee2433659f7465206bd43a9713e9
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-04-05 21:19:10 +0100

    :0

--- frontend/src/pages/Careers.tsx
+++ frontend/src/pages/Careers.tsx
@@ -366,7 +366,6 @@ const Careers: React.FC = () => {
             </div>
 
             <div className="careers-cta-wrapper" style={{ marginTop: "80px", textAlign: "center" }}>
-                <p style={{ marginBottom: "15px", color: "#555" }}>Don’t see your role? Send us a proposal anyway</p>
 
             </div>
             <Footer />
