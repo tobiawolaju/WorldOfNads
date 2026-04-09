b67de8b102aba4389fd6f1103526d3e1208fb1e4
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-04-09 20:56:41 +0100

    :l9

--- frontend/src/components/ThreeScene.tsx
+++ frontend/src/components/ThreeScene.tsx
@@ -159,7 +159,7 @@ const NadModel: React.FC<NadModelProps> = ({
       cube.scale.setScalar(0.015);
 
       // Applying Godot values: x:left, y:top, z:forward
-      cube.position.set(0, -2.25, 0.025);
+      cube.position.set(0, -2.25, 0.05);
 
       headBone.add(cube);
       console.log("Attached cube to bone:", headBone.name);
