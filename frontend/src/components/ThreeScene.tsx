22cbbe5ee3da1a87a73b2702d22be56341d0a6af
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-04-11 17:25:28 +0100

    :0

--- frontend/src/components/ThreeScene.tsx
+++ frontend/src/components/ThreeScene.tsx
@@ -374,7 +374,7 @@ const NadModel: React.FC<NadModelProps> = ({
       }
       const attachment = new THREE.Mesh(geometry, material);
       attachment.name = "bone-attachment";
-      attachment.scale.setScalar(0.018);
+      attachment.scale.setScalar(0.0165);
 
       // Positioning adjustment
       attachment.position.set(0, -2.25, -0.05);
