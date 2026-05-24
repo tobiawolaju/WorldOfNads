7e4012d57a78b0dbf7bef987d125fa89e42ddc42
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-05-24 22:54:31 +0100

    :0:-

--- frontend/src/pages/staticMatches.js
+++ frontend/src/pages/staticMatches.js
@@ -10,8 +10,10 @@ export const staticMatches = [
     time: "Live Now",
     date: "2026-03-09",
     startTime: 1741510800,
-    image: "https://pbs.twimg.com/profile_images/1861739634428174336/26FzLLyr.jpg",
-    description: "Nad Island is open 24/7 — sharpen your skills, learn the map, and stay ready. Server locks when the main event goes live and reopens once it's over.",
-    url: ""
-  }
+    image:
+      "https://pbs.twimg.com/profile_images/1861739634428174336/26FzLLyr.jpg",
+    description:
+      "Nad Island is open 24/7 — sharpen your skills, learn the map, and stay ready. Server locks when the main event goes live and reopens once it's over.",
+    url: "",
+  },
 ];
