8a7b27e07fa59d76ebf4bc1d19312e30d6834362
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-06-04 03:31:49 +0100

    updates

---
+++ contracts/src/WONsRegistry.sol
@@ -0,0 +1,3 @@
+Features:
+A simple directory. It stores the IPFS hash of your game frontend and the IP addresses of the Relay Nodes.
+At first, only you can update this. In WONs2, the DAO will update this.
